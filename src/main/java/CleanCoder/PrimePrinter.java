4b449780f3833c5e111ad8cbbe87bebf9efe7ed5
JovannyCortez <jovany.cortez@mercadolibre.com.mx>
2021-10-11 16:09:04 -0500

    PrimeGenerator

---
+++ src/main/java/CleanCoder/PrimePrinter.java
@@ -0,0 +1,15 @@
+package CleanCoder;
+
+public class PrimePrinter {
+    private static final int NUMBER_OF_PRIMES = 1000;
+    private static final int LINES_PER_PAGE = 50;
+    private static final int COLUMNS = 4;
+
+    public static void main(String[] args) {
+        final PrimeGenerator primeGenerator = new PrimeGenerator();
+        final NumberPrinter numberPrinter = new NumberPrinter(LINES_PER_PAGE, COLUMNS);
+
+        int[] primes = primeGenerator.generatePrimes(NUMBER_OF_PRIMES);
+        numberPrinter.print(primes, NUMBER_OF_PRIMES);
+    }
+}
